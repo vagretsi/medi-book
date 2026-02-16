5e228893bf721f7e8b838c8a18ae608798e318fd
Vasilis Gretsistas <vasilis.gretsistas@gmail.com>
2026-02-16 17:09:29 +0200

    middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,7 @@
-export { default } from "next-auth/middleware"
+import middleware from "next-auth/middleware";
+
+export default middleware;
+
 export const config = { 
   matcher: ["/((?!api/auth|login).*)"] 
-}
+};
